1bdf4ed923a66f87263b654c6d9a5fe45e9e45c9
isaactsg <idw.wismer@gmail.com>
2015-10-22 15:21:04 -0400

    17

--- Problem17.java
+++ Problem17.java
@@ -8,11 +8,13 @@ public class Problem17{
                 sum += count(i/100);
                 sum += "hundred".length();
                 sum += "and".length();
-
+                sum += tens(i/100);
+                sum += count(i % 10);
             } else if (i < 10){
                 sum += count(i);
             } else {
-
+                sum += tens(i);
+                sum += count(i % 10);
             }
         }
         System.out.println(sum);
@@ -38,6 +40,33 @@ public class Problem17{
                 return 5;
             case 9:
                 return 4;
+        }
+        return -1;
+    }
+
+    public static int tens(int num){
+        num /= 10;
+        switch (num){
+            case 1:
+                return "teen".length();
+            case 2:
+                return "twenty".length();
+            case 3:
+                return "thirty".length();
+            case 4:
+                return "forty".length();
+            case 5:
+                return "fifty".length();
+            case 6:
+                return "sixty".length();
+            case 7:
+                return "seventy".length();
+            case 8:
+                return "eighty".length();
+            case 9:
+                return "ninty".length();
+            case 0:
+                return 0;
         }
         return -1;
     }
