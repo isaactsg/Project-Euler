27871ebae1f3ecf1297493005f3684f87273280f
isaactsg <idw.wismer@gmail.com>
2015-10-23 10:24:54 -0400

    18

--- Problem16.java
+++ Problem16.java
@@ -1,5 +1,17 @@
+import java.math.BigInteger;
 public class Problem16{
     public static void main(String[] args){
-
+        int c = 1;
+        for(int i = 0; i < 1000; i++){
+            c = (c * 2) % 1000;
+        }
+        System.out.println(c);
+        BigInteger b = new BigInteger("2");
+        String s = b.pow(1000).toString();
+        int sum = 0;
+        for(int i = 0; i < s.length(); i++){
+            sum += Integer.parseInt(s.substring(i, i + 1));
+        }
+        System.out.println(sum);
     }
 }
--- Problem17.java
+++ Problem17.java
@@ -7,14 +7,17 @@ public class Problem17{
             } else if (i >= 100){
                 sum += count(i/100);
                 sum += "hundred".length();
-                sum += "and".length();
-                sum += tens(i/100);
-                sum += count(i % 10);
-            } else if (i < 10){
-                sum += count(i);
+                if (i % 100 != 0){
+                    sum += "and".length();
+                    sum += tens(i % 100);
+                }
+                //sum += count(i % 10);
             } else {
                 sum += tens(i);
-                sum += count(i % 10);
+                //sum += count(i % 10);
+            }
+            if(i < 1001){
+                System.out.println( i + " " + sum + " " + i/100 + " " + i%100);
             }
         }
         System.out.println(sum);
@@ -41,33 +44,69 @@ public class Problem17{
             case 9:
                 return 4;
         }
-        return -1;
+        return 0;
     }
 
     public static int tens(int num){
-        num /= 10;
-        switch (num){
-            case 1:
-                return "teen".length();
-            case 2:
-                return "twenty".length();
-            case 3:
-                return "thirty".length();
-            case 4:
-                return "forty".length();
-            case 5:
-                return "fifty".length();
-            case 6:
-                return "sixty".length();
-            case 7:
-                return "seventy".length();
-            case 8:
-                return "eighty".length();
-            case 9:
-                return "ninty".length();
-            case 0:
-                return 0;
+        if (num < 20 && num > 9){
+            switch (num) {
+                case 10:
+                    return 3;
+                case 11:
+                    return 5;
+                case 12:
+                    return 6;
+                case 13:
+                    return 8;
+                case 14:
+                    return 8;
+                case 15:
+                    return 7;
+                case 16:
+                    return 7;
+                case 17:
+                    return 9;
+                case 18:
+                    return 9;
+                case 19:
+                    return 8;
+            }
         }
-        return -1;
+        int sum = 0;
+        sum += count(num % 10);
+        switch (num / 10){
+            case 1:
+                sum += "teen".length();
+                System.out.println("This should never happen");
+                break;
+            case 2:
+                sum += "twenty".length();
+                break;
+            case 3:
+                sum += "thirty".length();
+                break;
+            case 4:
+                sum += "forty".length();
+                break;
+            case 5:
+                sum += "fifty".length();
+                break;
+            case 6:
+                sum += "sixty".length();
+                break;
+            case 7:
+                sum += "seventy".length();
+                break;
+            case 8:
+                sum += "eighty".length();
+                break;
+            case 9:
+                sum += "ninty".length();
+                break;
+            case 0:
+                sum += 0;
+                break;
+        }
+        return sum;
     }
 }
